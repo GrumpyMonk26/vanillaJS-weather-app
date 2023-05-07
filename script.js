240968557fb637777bf76d72833367fbed729465
Jason_Evans <evans.jason@hotmail.co.uk>
2023-05-07 21:36:27 +0100

    forecast coming from weather map api, error message displaying

---
+++ script.js
@@ -0,0 +1,51 @@
+const apiKey = '2f5e9dab20b292b0ccf09e1dbb2a1c8f';
+
+const weatherDataEl = document.getElementById('weather-data');
+const cityInputEl = document.getElementById('city-input');
+const formEl = document.querySelector('form');
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const locationValue = cityInputEl.value;
+  getWeatherData(locationValue);
+});
+
+async function getWeatherData(locationValue) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${locationValue}&appid=${apiKey}&units=metric`
+    );
+    if (!response.ok) {
+      throw new error('Network Response Was Not Ok');
+    }
+    const data = await response.json();
+    console.log(data);
+    const temprature = Math.round(data.main.temp);
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
+    const details = [
+      `Feels like: ${Math.round(data.main.feels_like)}℃`,
+      `Humidity: ${data.main.humidity}%`,
+      `Wind Speed: ${data.wind.speed}`,
+    ];
+    console.log(temprature);
+    console.log(description);
+    console.log(icon);
+    console.log(details);
+    weatherDataEl.querySelector('.icon').innerHTML = `<img
+            src="http://openweathermap.org/img/wn/${icon}.png"
+            alt="Weather Icon"
+          />`;
+    weatherDataEl.querySelector('.temperature').textContent = `${temprature} ℃`;
+    weatherDataEl.querySelector('.description').textContent = description;
+    weatherDataEl.querySelector('.details').innerHTML = details
+      .map((detail) => `<div>${detail}</div>`)
+      .join('');
+  } catch (error) {
+    weatherDataEl.querySelector('.icon').innerHTML = '';
+    weatherDataEl.querySelector('.temperature').textContent = '';
+    weatherDataEl.querySelector('.description').textContent =
+      'There has been an error, check location!';
+    weatherDataEl.querySelector('.details').innerHTML = '';
+  }
+}
